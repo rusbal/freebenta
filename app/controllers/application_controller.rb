6eebba57873ce86d0ba1671e927d8f71d614cd5a
Raymond Usbal <raymond@philippinedev.com>
2019-03-08 01:56:05 +0800

    Added pundit gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  include Pundit
+
+  rescue_from Pundit::NotAuthorizedError do
+    redirect_to root_url, alert: 'You do not have access to that page.'
+  end
 end
