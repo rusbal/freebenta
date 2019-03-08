14d0bad3544a028ffe66b80cdcbbc1bdcf931e9c
Raymond Usbal <raymond@philippinedev.com>
2019-03-08 22:35:19 +0800

    Refactor user seeder

--- db/seeds/00_users.rb
+++ db/seeds/00_users.rb
@@ -1,29 +1,10 @@
 
 require 'faker'
 
-root = [ 'raymond@philippinedev.com' ]
-admin = [
-  'admin@gmail.com',
-  'admin2@gmail.com',
-  'admin3@gmail.com'
-]
-advertiser = [
-  'ads@gmail.com',
-  'ads2@gmail.com',
-  'ads3@gmail.com'
-]
-commoner = [
-  'commoner@gmail.com',
-  'commoner2@gmail.com',
-  'commoner3@gmail.com',
-  'commoner4@gmail.com',
-  'commoner5@gmail.com',
-  'commoner6@gmail.com',
-  'commoner7@gmail.com',
-  'commoner8@gmail.com',
-  'commoner9@gmail.com',
-  'commoner10@gmail.com'
-]
+root       = [ 'raymond@philippinedev.com' ]
+admin      =  3.times.map { |n| "admin#{n + 1}@gmail.com" }
+advertiser =  3.times.map { |n| "ads#{n + 1}@gmail.com" }
+commoner   = 50.times.map { |n| "commoner#{n + 1}@gmail.com" }
 
 def create(email, type)
   user = User.new
@@ -37,9 +18,9 @@ def create(email, type)
 end
 
 if User.count == 0
-  root.map { |email| create(email, 'Root') }
-  admin.map { |email| create(email, 'Admin') }
-  advertiser.map { |email| create(email, 'Advertiser') }
-  commoner.map { |email| create(email, 'Commoner') }
+  root.map { |email| create(email, 'User::Root') }
+  admin.map { |email| create(email, 'User::Admin') }
+  advertiser.map { |email| create(email, 'User::Advertiser') }
+  commoner.map { |email| create(email, 'User::Commoner') }
 end
 
