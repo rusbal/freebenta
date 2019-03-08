fa86093d6e7829c17ea5ac73be1353b2a1936ed7
Raymond Usbal <raymond@philippinedev.com>
2019-03-09 04:55:12 +0800

    Set default_url_options on env production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'freebenta.philippinedev.com', port: 443 }
 end
+
+Rails.application.routes.default_url_options[:host] = 'freebenta.philippinedev.com'
+
