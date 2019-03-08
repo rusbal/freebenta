0671b34d10a760113680cd8f5c0acdd9a93d1ca1
Raymond Usbal <raymond@philippinedev.com>
2019-03-08 22:38:39 +0800

    Migration for item.user references

--- db/migrate/20190308070233_create_items.rb
+++ db/migrate/20190308070233_create_items.rb
@@ -1,7 +1,8 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.string  :type # ActiveItem, SoldItem
+      t.references :user, index: true
+      t.string  :type # Item::Active, Item::Sold, Item::Inactive, Item::Reserved, Item::Understock
       t.string  :name, null: false
       t.string  :description
       t.integer :price
