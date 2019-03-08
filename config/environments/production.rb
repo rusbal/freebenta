15d2e93609a5e634065fa5a0f72334028e3e1a98
Raymond Usbal <raymond@philippinedev.com>
2019-03-09 05:18:38 +0800

    Setup SMPT mailgun for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,16 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :authentication => :plain,
+    :address => "smtp.mailgun.org",
+    :port => 587,
+    :domain => "freebenta.philippinedev.com",
+    :user_name => "postmaster@freebenta.philippinedev.com",
+    :password => "b2420d2639026a06f65e4f93063fe31d-acb0b40c-b670261e"
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
