316c7f21a392ae632f68394702585b73287febd1
Raymond Usbal <raymond@philippinedev.com>
2019-03-08 18:53:32 +0800

    Echo seeding file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "Seeding..."
+
 Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].sort.each do |filename|
+  puts " #{filename}..."
   load(filename)
 end
 
