6a2b190a5e6b1cbb147748fde70b538feefbd658
Raymond Usbal <raymond@philippinedev.com>
2019-03-09 03:30:16 +0800

    Home page

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,10 @@
 module HomeHelper
+  def category_with_count(category)
+    "#{category.name} <span class='badge badge-light badge-sm'>#{category.items.count}</span>"
+  end
+
+  def format_price(item)
+    price = item.price / 100.0
+    number_to_currency price, unit: "₱"
+  end
 end
