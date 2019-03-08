0a745f85caad0f0850824e3b8dab29ddec7ab0c8
Raymond Usbal <raymond@philippinedev.com>
2019-03-09 03:29:43 +0800

    Routes update

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
 
-  resources :categories, param: :slug
+  namespace :admin do
+    get '/', to: 'dashboard#index'
+  end
+
+  resources :categories
+  resources :items
 end
