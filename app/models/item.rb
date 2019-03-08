b772ceaaa9fa3a05d98fcb2696792bb7921580d2
Raymond Usbal <raymond@philippinedev.com>
2019-03-08 22:37:40 +0800

    Updated scope

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,13 @@ class Item < ApplicationRecord
 
   has_one_attached :primary_image
   has_many_attached :images
+  belongs_to :commoner, class_name: 'User::Commoner', foreign_key: :user_id
 
-  scope :active, -> { where(type: 'ActiveItem') }
+  include Messageable
+
+  scope :active,     -> { where(type: 'Item::Active') }
+  scope :inactive,   -> { where(type: 'Item::Inactive') }
+  scope :sold,       -> { where(type: 'Item::Sold') }
+  scope :reserved,   -> { where(type: 'Item::Reserved') }
+  scope :understock, -> { where(type: 'Item::Understock') }
 end
