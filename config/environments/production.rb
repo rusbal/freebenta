534e774508977bb24d78c3a80e9d7dcf5cef6f71
Raymond Usbal <raymond@philippinedev.com>
2019-03-11 09:52:22 +0800

    mailer perform deliveries

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
 
   # SMTP [mailgun_smtp | gmail_smtp]
   smtp = Rails.application.credentials.mailgun_smtp
