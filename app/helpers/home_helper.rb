144b9de44261026cf67a7b88c453d1c9f652ae8d
Raymond Usbal <raymond@philippinedev.com>
2019-03-11 08:37:55 +0800

    slick banner carousel on item page

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -3,9 +3,9 @@ module HomeHelper
     "#{category.name} <span class='badge badge-light badge-sm'>#{category.items.count}</span>"
   end
 
-  def format_price(item)
+  def format_price(item, currency_symbol = '')
     price = item.price / 100.0
     #number_to_currency price, unit: "₱"
-    number_to_currency price, unit: ''
+    number_to_currency price, unit: currency_symbol
   end
 end
