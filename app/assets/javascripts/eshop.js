1e667f1b6334e97b218814c0a5e188cc9bbc5821
Raymond Usbal <raymond@philippinedev.com>
2019-03-13 01:31:40 +0800

    Use jquery3 and ujs on eshop

--- app/assets/javascripts/eshop.js
+++ app/assets/javascripts/eshop.js
@@ -1,6 +1,10 @@
-//= require eshop/jquery.min
+// require eshop/jquery.min
+//= require jquery3
+//= require jquery_ujs
 //= require eshop/bootstrap.min
 //= require eshop/slick.min
 //= require eshop/nouislider.min
 //= require eshop/jquery.zoom.min
 //= require eshop/main
+
+app.debugShow()
